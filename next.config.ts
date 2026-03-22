fc4c9370528946ef4b0d7be1b21b985565193901
Gunther Kroth <gdkroth@gmail.com>
2026-03-22 15:58:27 -0400

    add The Library to list of allowed image sources

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(`${process.env.R2_PUBLIC_URL}/**`)],
+    remotePatterns: [
+      new URL(`${process.env.R2_PUBLIC_URL}/**`),
+      new URL(`https://thelibrary1994.com/**`),
+    ],
   },
 };
 
