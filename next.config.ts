be9a2f7785a73cbff7da57fff67cb4edec8daccc
Gunther Kroth <gdkroth@gmail.com>
2026-05-06 19:52:31 -0400

    turn off image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       new URL(`${process.env.R2_PUBLIC_URL}/**`),
       new URL(`${process.env.R2_BACKGROUND_PUBLIC_URL}/**`),
